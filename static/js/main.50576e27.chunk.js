8a7c28f3f403c78bd19a86fba5bf9dbca7cc0d0c
Klirik <dasoftweb@gmail.com>
2021-04-23 13:54:47 +0300

    Updates

---
+++ static/js/main.50576e27.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);n(4);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=n(0),a=function(){return Object(s.jsx)(s.Fragment,{})};n(10);i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[11,1,2]]]);
+//# sourceMappingURL=main.50576e27.chunk.js.map
